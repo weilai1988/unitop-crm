ee0e940a8c71baa2df0d54f635a1f2c0f7664b2c
Tony Fu <tonyf@gorapid.com.au>
2017-11-27 18:06:46 +1100

    Add trainer

--- routes/views/about.js
+++ routes/views/about.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+var Trainer = keystone.list('Trainer');
 
 exports = module.exports = function (req, res) {
 
@@ -8,7 +9,16 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'about';
+	view.on('init', function (next) {
+		Trainer.model.find().exec(function (err, results) {
+			if (err || !results.length) {
+				return next(err);
+			}
 
+			locals.trainers = results;
+			next()
+		})
+	});
 	// Render the view
 	view.render('about');
 };
